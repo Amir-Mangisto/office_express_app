edaac4e2f97051ddc056e0e61067d7538d688494
Amir-Mangisto <mangisto.man@gmail>
2022-02-28 17:02:17 +0200

    aa

--- server/server.js
+++ server/server.js
@@ -1,18 +1,17 @@
 require('dotenv').config();
 const express =require('express');
-const cors=require('cors');
 require('./DB');//there is no need to write index.js because its default
+const cors=require('cors');
+const officeRouter = require('./router/office-router')
 const app=express();
+app.use(express.json())
 app.use(cors());
 const port = process.env.port;
-app.use(express.json())
-
-
-
-const officeRouter = require('./router/office-router')
+app.listen(port);
 app.use('/office',officeRouter);
-
 app.use('/',(req,res)=>{
     res.send({message:"server is online"})
 })
-app.listen(port);
+
+
+
