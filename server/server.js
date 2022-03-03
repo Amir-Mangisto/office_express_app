627fbd388b48bb98962226265a10c1903910154c
Amir-Mangisto <mangisto.man@gmail>
2022-03-03 12:31:22 +0200

    server change

--- server/server.js
+++ server/server.js
@@ -26,6 +26,6 @@ app.listen(port);
 
 //must be the last part of the code otherwise will not work
 if(process.env.NODE_ENV === 'production' ){
-    app.use(express.static('client/build'))
+    app.use(express.static(path.join(__dirname,'client/build')))
     app.get('*',(req,res)=>res.sendFile(path.join(__dirname,'../client/build','index.html')))
 }
